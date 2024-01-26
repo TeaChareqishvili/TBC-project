ec64c1b90b6a9ceb3c8dcc645b7071e29df33065
TeaChareqishvili <teachareqishvili89@gmail.com>
2024-01-26 18:31:23 +0400

    slider function

--- main.js
+++ main.js
@@ -240,23 +240,89 @@ const secondImage = arrowContainer.querySelector('.slide-arrow:nth-child(2)');
 secondImage.style.transform = 'rotate(180deg)'
 
 
-// slider timer 
+//  logic for slider  function 
+
 const sliderContainer = document.querySelector('.slider-container');
 const partnerSlides = document.querySelectorAll('.slider');
 let currentSlideIndex = 0;
+let slideIntervalId;
+
+// function for adding and removing active styles
 
 function showSlide(index) {
   partnerSlides.forEach((slide, i) => {
-    slide.classList.toggle('active', i === index);
-    slide.classList.toggle('inactive', i !== index);
+    if (i === index) {
+      slide.classList.add('active');
+      slide.classList.remove('inactive');
+    } else {
+      slide.classList.remove('active');
+      slide.classList.add('inactive');
+    }
   });
 }
+// function for slide changing 
 
 function changeSlide() {
   currentSlideIndex = (currentSlideIndex + 1) % partnerSlides.length;
   showSlide(currentSlideIndex);
+  updateDots(currentSlideIndex);
 }
 
+// function to change the slides with arrows
+
+function handleArrowClick(index) {
+  clearInterval(slideIntervalId);
+
+  if (index === 0) {
+    currentSlideIndex = (currentSlideIndex - 1 + partnerSlides.length) % partnerSlides.length;
+  } else {
+    currentSlideIndex = (currentSlideIndex + 1) % partnerSlides.length;
+  }
+
+  showSlide(currentSlideIndex);
+  updateDots(currentSlideIndex);
+  slideIntervalId = setInterval(changeSlide, 4000);
+}
+
+
+// function to change the slide with dots 
+
+function handleDotClick(dotIndex) {
+  clearInterval(slideIntervalId);
+  currentSlideIndex = dotIndex;
+  showSlide(currentSlideIndex);
+  updateDots(currentSlideIndex);
+  slideIntervalId = setInterval(changeSlide, 4000);
+}
+
+
+function updateDots(activeDotIndex) {
+  const dots = document.querySelectorAll('.dot');
+  dots.forEach((dot, index) => {
+    dot.classList.toggle('active', index === activeDotIndex);
+  });
+}
+
+
+//  event listener for arrows
+const arrows = document.querySelectorAll('.slide-arrow');
+arrows.forEach((arrow, index) => {
+  arrow.addEventListener('click', () => {
+    handleArrowClick(index);
+  });
+});
+
+
+// event listener for dots
+const dots = document.querySelectorAll('.dot');
+dots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    handleDotClick(index);
+  });
+});
+
+
 showSlide(currentSlideIndex);
-setInterval(changeSlide, 4000);  
+updateDots(currentSlideIndex);
+slideIntervalId = setInterval(changeSlide, 4000);
 
